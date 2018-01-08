7f490504a654ad4ac55a08e486be41c38ee76a63
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2018-01-08 20:45:14 -0200

    Rotas: Imperativas, Redirecionamento Via Código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { CursosComponent } from './cursos/cursos.component';
 import { routing } from './app.routing';
 import { MaterializeModule } from 'angular2-materialize';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos/cursos.service';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
 @NgModule({
   declarations: [
@@ -16,14 +18,17 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
     HomeComponent,
     LoginComponent,
     CursosComponent,
-    CursoDetalheComponent
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule, 
     routing,
     MaterializeModule
   ],
-  providers: [],
+  providers: [
+    CursosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
