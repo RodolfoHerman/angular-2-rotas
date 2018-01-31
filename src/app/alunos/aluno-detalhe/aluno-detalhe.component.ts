678f4174817003b864f8c74bb28ca71d19077de2
RodolfoHerman <ciencia.rodolfo@gmail.com>
2018-01-31 13:14:00 -0200

    Rotas: Usando Guarda de Rotas, Resolve - Carregando Dados Antes da Rota ser Ativada

--- src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
+++ src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs/Subscription';
 import { ActivatedRoute } from '@angular/router';
 import { AlunosService } from '../alunos.service';
 import { Router } from '@angular/router';
+import { Aluno } from "./../aluno";
 
 @Component({
   selector: 'app-aluno-detalhe',
@@ -11,7 +12,7 @@ import { Router } from '@angular/router';
 })
 export class AlunoDetalheComponent implements OnInit {
 
-  aluno: any;
+  aluno: Aluno;
   inscricao: Subscription;
 
   constructor(
@@ -21,12 +22,21 @@ export class AlunoDetalheComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.inscricao = this.route.params.subscribe(
-      (params: any) => {
+    // this.inscricao = this.route.params.subscribe(
+    //   (params: any) => {
 
-        let id = params['id'];
+    //     let id = params['id'];
 
-        this.aluno = this.alunosService.getAlunoId(id);
+    //     this.aluno = this.alunosService.getAlunoId(id);
+    //   }
+    // );
+
+    console.log('ngOnInit: AlunoDetalheComponent');
+
+    this.inscricao = this.route.data.subscribe(
+      (info: {aluno: Aluno}) => {
+        console.log('Objeto aluno resolver');
+        this.aluno = info.aluno;
       }
     );
   }
--- src/app/guards/alunos.guard.ts
+++ src/app/guards/alunos.guard.ts
@@ -14,6 +14,8 @@ export class AlunosGuard implements CanActivateChild {
         // console.log(route);
         // console.log(state);
 
+        console.log('AlunosGuard: rota filha');
+
         // if(state.url.includes('editar')) {
         //     alert("Usuário sem permissão");
         //     return Observable.of(false);
